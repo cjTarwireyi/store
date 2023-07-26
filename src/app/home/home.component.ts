8f9035aafe64837fa63a718bd317c41159d396ee
cornelious Tarwireyi <cornelioustarwireyi@gmail.com>
2023-07-26 20:21:44 +0200

    chaged landing page message

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
  templateUrl:'./home.component.html',
-  styles: [
-  ]
+ //Investigate why external css does not want to work
+ styles:[`
+ a:link {
+  color: blue;
+  background-color: transparent;
+  text-decoration: none;
+}
+a:visited {
+  color: pink;
+  background-color: transparent;
+  text-decoration: none;
+}
+a:hover {
+  color: red;
+  background-color: transparent;
+  text-decoration: underline;
+}
+a:active {
+  color: yellow;
+  background-color: transparent;
+  text-decoration: underline;
+}`]
 })
 
 export class HomeComponent {
