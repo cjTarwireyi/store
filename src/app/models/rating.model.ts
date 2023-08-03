2339221e5117046fe3e692628d600e41333e4719
cornelious Tarwireyi <cornelioustarwireyi@gmail.com>
2023-08-03 23:49:44 +0200

    star review calculation

---
+++ src/app/models/rating.model.ts
@@ -0,0 +1,7 @@
+export class IRatingModel{
+one: number = 0;
+two: number = 0;
+three: number = 0;
+four: number = 0;
+five: number =0;
+}
--- src/app/shared/star-rating/star-rating.component.ts
+++ src/app/shared/star-rating/star-rating.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { IRatingModel } from 'src/app/models/rating.model';
 
 @Component({
   selector: 'app-star-rating',
   templateUrl:'./star-rating.component.html',
   styles: [
+    `.crop {
+      overflow: hidden;
+    }
+    div {
+      cursor: pointer;
+    }`
   ]
 })
-export class StarRatingComponent {
+export class StarRatingComponent implements OnChanges, OnInit {
+
+
   cropWidth = 75;
   rating= 3;
-  
+  ratingVotes: IRatingModel ={
+    one: 1,
+    two: 0,
+    three: 0,
+    four: 1,
+    five: 1
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+     this.cropWidth = this.rating * 75 / 5;
+  }
+  ngOnInit(): void {
+    this.cropWidth = this.calculateAverageRating() * 75 / 5;
+  }
   onClick():void{
 
+  }
+  calculateAverageRating():number{
+    return (this.ratingVotes.one * 1 + this.ratingVotes.two * 2 + this.ratingVotes.three * 3 + this.ratingVotes.four * 4 + this.ratingVotes.five * 5)
+    /(this.ratingVotes.one + this.ratingVotes.two  + this.ratingVotes.three  + this.ratingVotes.four  + this.ratingVotes.five )
   }
 }
