a97f8dc4682b199ee8df16efea10c8d0c465393c
cornelious Tarwireyi <cornelioustarwireyi@gmail.com>
2023-08-12 20:58:56 +0200

    #8 styling site

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -18,7 +18,7 @@ export class ProductsComponent implements OnInit, OnDestroy {
   rowHeight= ROWS_HEIGHT[this.cols];
   category:string | undefined;
   sort ='desc';
-  count='5';
+  count='20';
   errorMessage=""; 
   filter :IFilter ={limit:"5", sort:"desc",category:undefined}
   loadByCategory = false;
@@ -84,11 +84,20 @@ export class ProductsComponent implements OnInit, OnDestroy {
       this.cols = 1;
     }else{
       this.cols = 3;
-    }
-    
+    }    
   }
-
   ngOnDestroy(): void {
  
   }
+  product : IProduct ={
+    id: 4,
+    title: "Canon EOS 400D",
+    image: "assets/dom-hill-nimElTcTNyY-unsplash.jpg",
+    description:"It is the successor of the Canon EOS 350D, and upgrades to a 10.1 megapixel CMOS sensor, a larger continuous shooting buffer, an integrated image sensor vibrating cleaning system (first used in a Canon EOS DSLR), a more precise nine-point autofocus system from the EOS 30D, improved grip, and a bigger 2.5-inch (64 mm) LCD with 230,000 pixels and a larger viewing angle which replaces the top status screen.",
+    price: 19.99,
+    category:"",
+    cropWidth:60,
+    rating:"5",
+    votes:100
+  } ;
 }
