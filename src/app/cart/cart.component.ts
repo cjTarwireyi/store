2612d372c2d59a99c301b012c922991030ad271e
cornelious Tarwireyi <cornelioustarwireyi@gmail.com>
2023-08-20 20:51:13 +0200

    categories styling fix

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -6,7 +6,8 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
-templateUrl:'./cart.component.html'
+templateUrl:'./cart.component.html',
+styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
    cart: ICart = {items:[
